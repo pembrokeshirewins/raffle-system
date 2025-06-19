f2f780bfec0b5bc633a5167f063e4ff5f395b2cc
pembrokeshirewins <stardom03.fibrin@icloud.com>
2025-06-19 17:32:01 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,7 @@
-root@5aaa18ef-dd89-4e8f-bb97-15568485d7b3:~/.daytona# cd /workspace && cat raffl
-e-system/next.config.ts
 import type { NextConfig } from "next";
- 
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
- 
+
 export default nextConfig;
-root@5aaa18ef-dd89-4e8f-bb97-15568485d7b3:/workspace#
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-Command
