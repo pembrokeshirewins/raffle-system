87416bbab4e2db8c8800e2c79f82c581947fabfb
pembrokeshirewins <stardom03.fibrin@icloud.com>
2025-06-19 18:29:40 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,3 @@
-root@5aaa18ef-dd89-4e8f-bb97-15568485d7b3:~/.daytona# cd /workspace && cat raffl
-e-system/src/app/layout.tsx
-import type { Metadata } from "next";
 // import localFont from "next/font/local";
 import "./globals.css";
  
